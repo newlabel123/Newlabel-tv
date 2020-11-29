c0a14349223d1eac9e73e098238677800ac3b8bf
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2020-11-29 08:52:12 +0100

    feat: 🎸 Episode Box Transition Update

--- src/components/tvshow/Episode.jsx
+++ src/components/tvshow/Episode.jsx
@@ -1,32 +1,39 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack, Icon, Image, Text } from '@chakra-ui/react'
 import styled from '@emotion/styled'
+import { BiCart } from 'react-icons/bi'
+import { BsClockHistory } from 'react-icons/bs'
 import React from 'react'
 
 import playBox from '../../assets/icons/Play.svg'
 
 function Episode ({ shot }) {
   return (
-    <EpisodeBox>
-      <div className="thumbnail">
-        <div className="overlay" />
-        <img className="scene" src={shot} alt="episode" />
-        <img className="play" src={playBox} alt="" />
-      </div>
-      <div className="details">
-        <h3>Episode 1</h3>
-        <p>
-          Three weeks, six countries - one divorce? Sometimes you need
-          to spend time together
-        </p>
-        <hr />
-        <div className="time">
-          <p>59 mins</p>
-          <div></div>
-          <p>$2.99</p>
-          <div></div>
-          <p>Available for 48hrs</p>
-        </div>
-      </div>
+    <EpisodeBox pt="75%" pos="relative" transition="all .5s">
+      <Box pos="absolute" w="100%" h="100%" top="0" left="0" borderRadius=".5rem" zIndex="-1" className="thumbnail">
+        <Box pos="absolute" top="0" left="0" w="100%" h="100%" bg="linear-gradient(180deg, rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.9) 100%)" opacity="0" transition="all .5s" borderRadius=".5rem" zIndex="9" className="overlay" />
+        <Image h="70%" w="100%" objectFit="cover" objectPosition="center" transition="all .5s" borderRadius=".5rem" className="scene" src={shot} alt="episode" />
+        <Box pos="absolute" left="0" bottom="0" transition="all .5s" zIndex="10" className="details">
+          <Image w="50px" opacity="0" className="play" src={playBox} alt="" />
+          <Text as="h3" fontWeight="bold" mt="1rem" mb=".5rem">Episode 1</Text>
+          <Text fontSize="1.2rem">
+            Three weeks, six countries - one divorce? Sometimes you need
+            to spend time together
+          </Text>
+          <HStack align="center" mt=".5rem">
+            <HStack>
+              <Icon color="#fff" as={BiCart} />
+              <Text color="#fff" fontSize="1rem">$35</Text>
+            </HStack>
+            <Box color="#fff" fontWeight="800" fontSize="1.2rem" mx=".5rem">.</Box>
+            <HStack>
+              <Icon color="#fff" as={BsClockHistory} />
+              <Text color="#fff" fontSize="1.2rem">2hrs 10mins</Text>
+            </HStack>
+            <Box color="#fff" fontWeight="800" fontSize="1.2rem" mx=".5rem">.</Box>
+            <Text color="#fff" fontSize="1.2rem">Available for 48hrs</Text>
+          </HStack>
+        </Box>
+      </Box>
     </EpisodeBox>
   )
 }
@@ -34,17 +41,11 @@ function Episode ({ shot }) {
 export { Episode }
 
 const EpisodeBox = styled(Box)`
-  /* padding-top: 56.25%; */
-  border-top: none;
-  transition: all 0.5s;
-
-  &::first-of-type {
-    margin-left: none;
-  }
 
   &:hover {
     img.scene {
       transform: scale(1.05);
+      height: 100%;
     }
 
     img.play {
@@ -53,99 +54,12 @@ const EpisodeBox = styled(Box)`
 
     .details {
       transform: translateY(-5px);
-    }
-
-    .time {
-      opacity: 1;
+      color: #fff;
     }
 
     .overlay {
       opacity: 1;
+      transform: scale(1.05)
     }
-
-    hr {
-      opacity: 1;
-    }
-  }
-
-  .thumbnail {
-    width: 100%;
-    height: 170px;
-    position: relative;
-    border-radius: 1rem;
-    z-index: -1;
-  }
-
-  img.scene {
-    width: 100%;
-    height: 100%;
-    object-position: center;
-    object-fit: cover;
-    transition: all 0.5s;
-    border-radius: 1rem;
-  }
-
-  img.play {
-    opacity: 0;
-    width: 50px;
-    position: absolute;
-    left: 0rem;
-    bottom: 1rem;
-    transition: all 0.5s;
-    z-index: 10;
-  }
-
-  .overlay {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    transform: scale(1.05);
-    background: linear-gradient(
-      180deg,
-      rgba(255, 255, 255, 0) 50%,
-      rgba(0, 0, 0, 0.9) 100%
-    );
-    opacity: 0;
-    transition: 0.5s;
-    border-radius: 1rem;
-    z-index: 9;
-  }
-
-  .details {
-    transition: all 0.5s;
-  }
-
-  .time {
-    opacity: 0;
-    align-items: center;
-    display: flex;
-    transition: all 0.5s;
-
-    div {
-      width: 1px;
-      height: 1rem;
-      margin: 0 1rem;
-      background: #000;
-    }
-  }
-
-  hr {
-    margin: 1.2rem 0;
-    width: 30px;
-    background: red;
-    transition: all 0.5s;
-    opacity: 0;
-  }
-
-  h3 {
-    margin: 1rem 0 0.4rem;
-    font-weight: bold;
-  }
-
-  p {
-    font-size: 1.2rem;
   }
 `
