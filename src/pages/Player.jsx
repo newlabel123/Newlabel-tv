8747537811e47bfb5a3f941101d8edd631ea62c5
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2021-01-15 15:00:53 +0100

    feat: 🎸 Fixed player size

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -4,23 +4,15 @@ import ReactPlayer from 'react-player'
 
 function Player() {
   return (
-    <Box w="100%" h="100%" overflow="hidden">
-      <AspectRatio ratio={16 / 9}>
-        <iframe
-          src="https://player.vimeo.com/video/290825945"
-          width="100%"
-          height="100%"
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-        {/* <ReactPlayer
-          url="https://vimeo.com/290825945"
-          width="100%"
-          height="70%"
-          playing={true}
-        /> */}
-      </AspectRatio>
+    <Box w="100%" h="100%">
+      <iframe
+        src="https://player.vimeo.com/video/290825945"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </Box>
   )
 }
