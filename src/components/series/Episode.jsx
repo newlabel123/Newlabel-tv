10f3b262324586bfaf679b44b3687a678ede542c
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2021-01-27 22:41:59 +0100

    feat: 🎸 Custom video player

--- src/components/series/Episode.jsx
+++ src/components/series/Episode.jsx
@@ -1,114 +1,119 @@
-import { Box, HStack, Icon, Image, Text } from '@chakra-ui/react'
+import React from 'react'
 import styled from '@emotion/styled'
+import { Box, HStack, Icon, Image, Text } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import { BiCart } from 'react-icons/bi'
 import { BsClockHistory } from 'react-icons/bs'
-import React from 'react'
 
 import playBox from '../../assets/icons/Play.svg'
 
 function Episode({ episode }) {
   return (
-    <EpisodeBox pt="75%" pos="relative" transition="all .5s">
-      <Box
-        pos="absolute"
-        w="100%"
-        h="100%"
-        top="0"
-        left="0"
-        borderRadius=".5rem"
-        zIndex="-1"
-        className="thumbnail"
-      >
+    <Link to="/player">
+      <EpisodeBox pt="75%" pos="relative" transition="all .5s">
         <Box
           pos="absolute"
-          top="0"
-          left="0"
           w="100%"
           h="100%"
-          bg="linear-gradient(180deg, rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.9) 100%)"
-          opacity="0"
-          transition="all .5s"
-          borderRadius=".5rem"
-          zIndex="9"
-          className="overlay"
-        />
-        <Image
-          h="69%"
-          w="100%"
-          objectFit="cover"
-          objectPosition="center"
-          transition="all .5s"
-          borderRadius=".5rem"
-          className="scene"
-          src={episode.scene.url}
-          alt="episode"
-        />
-        <Box
-          pos="absolute"
+          top="0"
           left="0"
-          bottom="0"
-          transition="all .5s"
-          zIndex="10"
-          className="details"
+          borderRadius=".5rem"
+          zIndex="-1"
+          className="thumbnail"
         >
-          <Image w="50px" opacity="0" className="play" src={playBox} alt="" />
-          <Text as="h3" fontWeight="bold" mt="1rem" mb=".5rem">
-            Episode {episode.episodeNumber}
-          </Text>
-          <Text fontSize="1.2rem">
-            Three weeks, six countries - one divorce? Sometimes you need to
-            spend time together
-          </Text>
-          <HStack align="center" mt=".5rem">
-            <HStack>
-              <Icon color="#fff" as={BiCart} />
-              <Text color="#fff" fontSize="1.2rem">
-                ${episode.buyPrice}
-              </Text>
-            </HStack>
-            <Box color="#fff" fontWeight="800" fontSize="1.2rem" mx=".5rem">
-              .
-            </Box>
-            <HStack>
-              <Icon color="#fff" as={BsClockHistory} />
-              <Text color="#fff" fontSize="1.2rem">
-                {episode.runtime}
-              </Text>
-            </HStack>
-            <Box color="#fff" fontWeight="800" fontSize="1.2rem" mx=".5rem">
-              .
-            </Box>
-            {/* <Text color="#fff" fontSize="1.2rem">
+          <Box
+            pos="absolute"
+            top="0"
+            left="0"
+            w="100%"
+            h="100%"
+            bg="linear-gradient(180deg, rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.9) 100%)"
+            opacity="0"
+            transition="all .5s"
+            borderRadius=".5rem"
+            zIndex="9"
+            className="overlay"
+          />
+          <Image
+            h="69%"
+            w="100%"
+            objectFit="cover"
+            objectPosition="center"
+            transition="all .5s"
+            borderRadius=".5rem"
+            className="scene"
+            src={episode.scene.url}
+            alt="episode"
+          />
+          <Box
+            pos="absolute"
+            left="0"
+            bottom="0"
+            transition="all .5s"
+            zIndex="10"
+            className="details"
+          >
+            <Image w="50px" opacity="0" className="play" src={playBox} alt="" />
+            <Text as="h3" fontWeight="bold" mt="1rem" mb=".5rem">
+              Episode {episode.episodeNumber}
+            </Text>
+            <Text fontSize="1.2rem">
+              Three weeks, six countries - one divorce? Sometimes you need to
+              spend time together
+            </Text>
+            <HStack align="center" mt=".5rem">
+              <HStack>
+                <Icon color="#fff" as={BiCart} />
+                <Text color="#fff" fontSize="1.2rem">
+                  ${episode.buyPrice}
+                </Text>
+              </HStack>
+              <Box color="#fff" fontWeight="800" fontSize="1.2rem" mx=".5rem">
+                .
+              </Box>
+              <HStack>
+                <Icon color="#fff" as={BsClockHistory} />
+                <Text color="#fff" fontSize="1.2rem">
+                  {episode.runtime}
+                </Text>
+              </HStack>
+              <Box color="#fff" fontWeight="800" fontSize="1.2rem" mx=".5rem">
+                .
+              </Box>
+              {/* <Text color="#fff" fontSize="1.2rem">
               Available for 48hrs
             </Text> */}
-          </HStack>
+            </HStack>
+          </Box>
         </Box>
-      </Box>
-    </EpisodeBox>
+      </EpisodeBox>
+    </Link>
   )
 }
 
 export { Episode }
 
 const EpisodeBox = styled(Box)`
-  &:hover {
-    img.scene {
-      transform: scale(1.05);
-      height: 100%;
-    }
+  @media (min-width: 768px) {
+    &:hover {
+      img.scene {
+        transform: scale(1.05);
+        height: 100%;
+      }
 
-    img.play {
-      opacity: 1;
-    }
+      img.play {
+        opacity: 1;
+      }
 
-    .details {
-      transform: translateY(-5px);
-      color: #fff;
-    }
+      .details {
+        transform: translateY(-5px);
+        color: #fff;
+      }
 
-    .overlay {
-      opacity: 1;
-      transform: scale(1.05);
+      .overlay {
+        opacity: 1;
+        transform: scale(1.05);
+      }
     }
   }
 `
--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,21 +1,119 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack, Input } from '@chakra-ui/react'
 import styled from '@emotion/styled'
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import ReactPlayer from 'react-player'
+
+import {
+  BsPlay,
+  BsPause,
+  BsArrowClockwise,
+  BsArrowCounterclockwise,
+  BsVolumeUp,
+} from 'react-icons/bs'
 
 import trailer from '../assets/videos/trailer2.mp4'
 
 function Player() {
+  const [playing, setPlaying] = useState(false)
+  const [volume, setVolume] = useState(1)
+  const [played, setPlayed] = useState(0)
+  const [seeking, setSeeking] = useState(false)
+  const playerRef = useRef(null)
+
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10)
+  }
+
+  const handleForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10)
+  }
+
+  const adjustVolume = ({ target }) => {
+    setVolume(target.value / 100)
+  }
+
+  const handleProgress = (e) => {
+    if (!seeking) {
+      setPlayed(e.played)
+    }
+  }
+
+  const seek = ({ target }) => {
+    setPlayed(parseFloat(target.value / 100))
+  }
+
+  const handleSeekStart = ({ target }) => {
+    setSeeking(true)
+  }
+
+  const handleSeekRelease = ({ target }) => {
+    setSeeking(false)
+    playerRef.current.seekTo(target.value / 100, 'fraction')
+  }
+
   return (
     <Wrapper>
-      <Box className="video-container">
-        <video
-          controls
-          id="trailer"
-          style={{ width: '100%', height: '100%', borderRadius: '5px' }}
-        >
-          <source src={trailer} type="video/mp4" />
-          Your browser does not support HTML5 video.
-        </video>
+      <Box className="video-container" color="brand.red">
+        <ReactPlayer
+          ref={playerRef}
+          playing={playing}
+          volume={volume}
+          onProgress={handleProgress}
+          url={trailer}
+        />
+
+        <Box px="2rem" w="100%" pos="absolute" bottom="3rem">
+          <Input
+            type="range"
+            min={0}
+            max={100}
+            value={played * 100}
+            onChange={seek}
+            onMouseDown={handleSeekStart}
+            onMouseUp={handleSeekRelease}
+            borderRadius="2rem"
+            w="100%"
+            h=".4rem"
+            border="none"
+            outline="none"
+            background="#f00"
+            cursor="pointer"
+            _focus={{
+              outline: 'none',
+              border: 'none',
+            }}
+          />
+
+          <HStack mt="1.5rem" transform="translateX(-.7rem)">
+            <Box
+              fontSize="3rem"
+              background="transparent"
+              onClick={() => setPlaying(!playing)}
+            >
+              {playing ? <BsPause /> : <BsPlay />}
+            </Box>
+            <HStack fontSize="2rem" spacing="1rem">
+              <BsVolumeUp />
+              <Input
+                borderRadius="2rem"
+                w="100px"
+                h=".3rem"
+                border="none"
+                outline="none"
+                type="range"
+                background="#f00"
+                cursor="pointer"
+                onChange={adjustVolume}
+                _focus={{
+                  outline: 'none',
+                  border: 'none',
+                }}
+              />
+              <BsArrowClockwise onClick={handleForward} />
+              <BsArrowCounterclockwise onClick={handleRewind} />
+            </HStack>
+          </HStack>
+        </Box>
       </Box>
     </Wrapper>
   )
